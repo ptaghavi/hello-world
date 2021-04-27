edaf36ef5002c29c66bfce7ae934e576d9fe315a
Pouyan Taghavi <pouyan.taghavi@pcc.edu>
2021-04-27 01:06:48 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,5 @@
 package edu.pcc.cis.helloworld;
-
+import java.util.Date;
 /**
  * Created by Pouyan.Taghavi
  * 04-26-2021
@@ -8,5 +8,7 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
+
     }
 }
