0157b1530e62d4d5de106d924e10ae1f9e8436b0
Pouyan Taghavi <pouyan.taghavi@pcc.edu>
2021-04-27 01:03:08 -0700

    added main method printing Hello, World

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,12 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Pouyan.Taghavi
+ * 04-26-2021
+ */
+
 public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
